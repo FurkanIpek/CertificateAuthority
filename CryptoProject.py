9ff25e98a33625aa0a0bfe64157dce998af0040a
Mehmet Furkan İpek <m.f.i_93@hotmail.com>
2015-12-02 23:32:08 +0200

    RSA encryption - decryption ready, yet not tested. CryptoPy integrated.

--- CryptoProject.py
+++ CryptoProject.py
@@ -1,7 +1,37 @@
 ﻿
 import utils
+import RSA
+
+from binascii import unhexlify
+
+from Crypto.Hash import SHA256
 
 if __name__ == "__main__":
-    print "Hello World!"
-    print utils.rm_primality(1486827721469542863220036771024176715261279068721829501153053826100543027714474985054200558646696929104909904109238728826303362614278367106731928491473671)
-    print utils.gen_random(1024)
+    message = "Hey there! The angel from my nightmare!"
+
+    #result = utils.AESencryption(message, 1)
+    #ori_m = utils.AESdecryption(result[0], result[1], result[2])
+    #print ori_m
+
+    #result = utils.DES3encryption(message)
+    #ori_m = utils.DES3decryption(result[0], result[1], result[2])
+    #print ori_m
+
+    #hash1 = utils.generateHash(message, 1)
+    #print hash1
+
+    #bigint = utils.gen_random(1024)
+    #print utils.rm_primality(bigint)
+    #print "to str: ", len(str(bigint))
+
+    #os2ip = RSA.OS2IP("123123")
+    #i2osp = RSA.I2OSP(os2ip, 6)
+
+    #print os2ip, i2osp
+
+    keys = RSA.generateKeys(1)
+    e, N, d, p, q = keys[0], keys[1], keys[2], keys[3], keys[4]
+    phi_N = (p-1) * (q-1)
+
+    print N == p * q
+    print (e * d) % phi_N
---
+++ RSA.py
@@ -0,0 +1,162 @@
+﻿import utils
+
+from random import randint
+from fractions import gcd
+
+def generateKeys(security_level): ## TODO sec_level & how to generate keys?
+    p = utils.gen_random(512)
+
+    while utils.rm_primality(p) == False:
+        p = utils.gen_random(512)
+
+    q = utils.gen_random(512)
+
+    while utils.rm_primality(p) == False:
+        q = utils.gen_random(512)
+
+    N = p * q
+
+    phi_N = (p-1) * (q-1)
+
+    e = randint(1, phi_N - 1)
+
+    while gcd(e, phi_N) != 1:
+        e = randint(1, phi_N - 1)
+
+    d = utils.inverse(e, phi_N)
+
+    return e, N, d, p, q
+
+def OS2IP(str): # TODO
+    length = len(str)
+    value = 0
+
+    for i in range(length):
+        value += int(str[i]) * pow(256, i)
+
+    return value
+
+
+def RSAEP(N, e, m):
+    if m < 0 or m > n - 1:
+        print "Message representative out of range!"
+        return -1
+
+    c = utils.mod_exp(m, e, N)
+
+    return c
+
+
+def I2OSP(x, xLen): #TODO
+    if x >= pow(256, xLen):
+        print "Integer too large"
+        return -1
+
+    arr = [ 0 for i in range(xLen) ]
+
+    for i in range(xLen):
+        arr[i] = x / pow(256, xLen-i)
+
+    output = ""
+
+    for i in range(xLen):
+        output += str(arr[i])
+
+    return output
+
+
+def MGF(mgfSeed, maskLen):
+    if maskLen > pow(2, 32):
+        print "Mask too long"
+        return
+
+    hLen = len(utils.generateHash("length", 1))
+    T = ""
+
+    for i in range(maskLen/hLen - 1):
+        C = I2OSP(i, 4)
+        T += utils.generateHash(mgfSeed + C)
+
+    output = ""
+
+    for i in range(maskLen):
+        output += T[i]
+
+    return output
+
+
+def encrypt(N, e, message, security_level, L = ""):
+    lHash = utils.generateHash(L, 1) # TODO lHash = hash(L)
+    hLen = len(lHash)
+    mLen = len(message)
+    k = len(str(N())) # TODO check correctness # k = octet length of modulus N
+
+    PS = ""
+    for i in range(k - mLen - 2*hLen - 2):
+        PS += "0"
+
+    DB = lHash + PS + chr(1) + message # TODO 0x01 == chr(1) ?
+
+    seed = ""
+    for i in range(hLen):
+        seed += chr(randint(0, 255))
+
+    dbMask = MGF(seed, k - hLenn - 1)
+    maskedDB = DB ^ dbMask
+
+    seedMask = MGF(maskedDB, hLen)
+    maskedSeed = seed ^ seedMask
+
+    encryptedMessage = chr(0) + maskedSeed + maskedDB # TODO 0x00 == chr(0) ?
+
+    m = OS2IP(encryptedMessage)
+
+    c = RSAEP(N, e, m)
+
+    cipherText = I2OSP(c, k)
+
+    return cipherText
+
+
+
+def RSADP(N, d, c):
+    if c < 0 or c > n - 1:
+        print "Ciphertext representative out of range"
+        return -1
+
+    m = utils.mod_exp(c, d, N)
+
+    return m
+
+
+def decrypt(N, d, p, q, cipher_text, L = ""):
+     k = len(str(N))
+     hLen = len(utils.generateHash("length", 1))
+
+     if k != len(cipher_text) or k < 2*hLen + 2:
+         print "Decryption error"
+         return -1
+
+     c = OS2IP(cipher_text)
+
+     m = RSADP(N, d, c)
+
+     EM = I2OSP(m, k)
+
+     if EM[0] != chr(0) or EM[1] != chr(1): # TODO again chr(0)
+         print "decryption error"
+         return -1
+
+     pos = EM.find(chr(0), 2) ## TODO -> this seeks 0 after 2nd position, to find M
+
+     message = EM[pos:]
+
+     return message
+
+
+def generateSignature():
+     print "TODO"
+
+
+def verifySignature():
+     print "TODO"
